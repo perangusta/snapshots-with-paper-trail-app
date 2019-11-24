1cc90f70c472c2d287b30f0380761832ad51438c
Jean-Michel Gigault <jgigault@student.42.fr>
2019-11-24 15:00:54 +0100

    Allow to filter on snapshots in dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,19 @@
 class HomeController < ApplicationController
   def index
-    @projects_summary_completed     = Project.summary(scope: :completed)
-    @projects_summary_draft         = Project.summary(scope: :draft)
-    @negotiations_summary_completed = Negotiation.summary(scope: :completed)
-    @negotiations_summary_draft     = Negotiation.summary(scope: :draft)
+    @snapshot_at = params[:snapshot_at].present? ? Date.parse(params[:snapshot_at]) : Date.current
+
+    project_collection     = Project.all
+    negotiation_collection = Negotiation.all
+
+    if @snapshot_at < Date.current
+      project_collection     = project_collection.snapshot_at(@snapshot_at.to_time.end_of_day)
+      negotiation_collection = negotiation_collection.snapshot_at(@snapshot_at.to_time.end_of_day)
+    end
+
+    @projects_summary_completed     = project_collection.summary(scope: :completed)
+    @projects_summary_draft         = project_collection.summary(scope: :draft)
+    @negotiations_summary_completed = negotiation_collection.summary(scope: :completed)
+    @negotiations_summary_draft     = negotiation_collection.summary(scope: :draft)
 
     @versions = PaperTrail::Version.all
   end
